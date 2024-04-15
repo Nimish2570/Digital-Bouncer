d55f8e66aa93bc6aae4ba65ad592c366c53ab136
Nimish2570 <2000560100075@bbdniit.ac.in>
2024-04-15 21:44:37 +0530

    register fixed 1

--- frontend/src/components/User/LoginSignUp.js
+++ frontend/src/components/User/LoginSignUp.js
@@ -99,35 +99,88 @@ const LoginSignUp = () => {
   return (
   
 
-<Fragment>
-  {loading ? <Loader/> : 
-  <Fragment>
-    {/* Warning message */}
-    <div style={{ color: 'red', marginBottom: '10px' }}>
-      Use images with size less than 1MB only to avoid internal server error.
-    </div>
+    <Fragment>
+        {loading ? <Loader/> : 
+        <Fragment>
+        <div className="LoginSignUpContainer">
+            <div className='LoginSignUpBox'>
+                <div>
+                    <div className='login_signUp_toggle'>
+                        <p onClick={(e)=> switchTabs(e,"login")}>Login</p>
+                        <p onClick={(e)=> switchTabs(e,"register")}>Register</p>
+                    </div>
+                    <button ref ={switcherTab}></button>
+                </div>
+                <form className='loginForm' ref={loginTab} onSubmit={loginSubmit}>
+                    <div className='LoginEmail'>
+                        <MailOutlineIcon />
+                        <input type='email' placeholder='Email' required  value={loginEmail} onChange={(e)=> setLoginEmail(e.target.value)}/>
+                    </div>
+                    <div className='LoginPassword'>
+                        <LockOutlinedIcon />
+                        <input type='password' placeholder='Password' required value={loginPassword} onChange={(e)=> setLoginPassword(e.target.value)}/>
+                    </div>
+                    <Link to='/password/forgot' className='forgotPassword'>Forgot Password?</Link>
 
-    <div className="LoginSignUpContainer">
-      <div className='LoginSignUpBox'>
-        <div>
-          <div className='login_signUp_toggle'>
-            <p onClick={(e)=> switchTabs(e,"login")}>Login</p>
-            <p onClick={(e)=> switchTabs(e,"register")}>Register</p>
-          </div>
-          <button ref ={switcherTab}></button>
+                    <input type='submit' value="Login"  className='loginBtn' ></input>
+
+                </form>
+
+                <form className='signUpForm' ref={registerTab} encType='multipart/form-data' onSubmit={registerSubmit}>
+                <div className="signUpName">
+                  <FaceIcon />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="name"
+                    value={name}
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <div className="signUpEmail">
+                  <MailOutlineIcon />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <div className="signUpPassword">
+                  <LockOpenIcon />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    name="password"
+                    value={password}
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <div style={{ color: 'red', marginBottom: '10px' }}>
+                  Use images with size less than 1MB only to avoid internal server error.
+                </div>
+
+                <div id="registerImage">
+                  <img src={avatarPreview} alt="Avatar Preview" />
+                  <input
+                    type="file"
+                    name="avatar"
+                    accept="image/*"
+                    onChange={registerDataChange}
+                  />
+                </div>
+                <input type="submit" value="Register" className="signUpBtn"  />
+              </form>
+            </div>
+
         </div>
-        <form className='loginForm' ref={loginTab} onSubmit={loginSubmit}>
-          {/* Login form inputs */}
-        </form>
 
-        <form className='signUpForm' ref={registerTab} encType='multipart/form-data' onSubmit={registerSubmit}>
-          {/* Registration form inputs */}
-        </form>
-      </div>
-    </div>
-  </Fragment>}
-</Fragment>
-   
+    </Fragment>}
+    </Fragment>    
   )
 }
 
